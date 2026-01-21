d60af95d3176d0306dc1c3b1048e71b00d6ee281
Aldrich Reboja <aktreboja@gmail.com>
2026-01-21 12:10:57 -0800

    Registration to Mongo

--- app/getting-started/page.tsx
+++ app/getting-started/page.tsx
@@ -1,4 +1,3 @@
-
 import ProtectedPageLayout from '../../components/Auth/ProtectedPageLayout';
 import { Button, Field, Input, Stack } from '@chakra-ui/react';
 import { auth0 } from '../../lib/auth0';
@@ -9,36 +8,10 @@ export default async function GettingStarted() {
   const session = await auth0.getSession();
   const user = session?.user;
 
-
-  const handleRegistration = async () => {
-    "use server";
-    try {
-      const response = await fetch('/api/registration', {
-        method: 'POST',
-        body: JSON.stringify({ email: user?.email, name: user?.name, username: user?.nickname }),
-      });
-      if (response.ok) {
-        toaster.create({
-          title: 'Registration Successful',
-          description: 'You have been registered successfully',
-          type: 'success',
-        })
-      }
-    } catch (error) {
-      console.error('Registration error:', error);
-      toaster.create({
-        title: 'Registration Failed',
-        description: 'An error occurred while registering',
-        type: 'error',
-      })
-    }
-    // redirect('/');
-  }
-
   return (
     <ProtectedPageLayout>
-      <div className = " mx-auto min-h-screen flex items-center justify-center flex-col">
-        <h1 className = "text-2xl font-bold text-center">Getting Started</h1>
+      <div className=" mx-auto min-h-screen flex items-center justify-center flex-col">
+        <h1 className="text-2xl font-bold text-center">Getting Started</h1>
 
         <GettingStartedForm user={user} />
       </div>
--- components/Auth/GettingStartedForm.tsx
+++ components/Auth/GettingStartedForm.tsx
@@ -1,41 +1,94 @@
-"use client";
-import { User } from "@auth0/nextjs-auth0/types";
-import { Button, Field, Input, Stack } from "@chakra-ui/react";
-import { toaster } from "../ui/toaster";
+'use client';
+import { User } from '@auth0/nextjs-auth0/types';
+import { Button, Field, Input, Stack } from '@chakra-ui/react';
+import { toaster, Toaster } from '../ui/toaster';
+import { useRouter } from 'next/navigation';
+import { useForm } from 'react-hook-form';
+import { z } from 'zod';
+import { zodResolver } from '@hookform/resolvers/zod';
 
-export default function GettingStartedForm({ user }: { user: User | undefined}) {
+const formSchema = z.object({
+  email: z.email({ message: 'Invalid email address' }),
+  name: z.string().min(1, { message: 'Name is required' }),
+  username: z.string().min(1, { message: 'Username is required' }),
+});
 
+export default function GettingStartedForm({
+  user,
+}: {
+  user: User | undefined;
+}) {
+  const router = useRouter();
+  const { register, handleSubmit } = useForm<z.infer<typeof formSchema>>({
+    defaultValues: {
+      email: user?.email,
+      name: user?.name,
+      username: user?.nickname,
+    },
+    resolver: zodResolver(formSchema),
+  });
 
-    const handleRegistration = async () => {
-        try {
-            const response = await fetch('/api/registration', {
-                method: 'POST',
-                body: JSON.stringify({ email: user?.email, name: user?.name, username: user?.nickname }),
-            });
-        }
-        catch (error) {
-            console.error('Registration error:', error);
-            toaster.create({
-                title: 'Registration Failed',
-                description: 'An error occurred while registering',
-                type: 'error',
-            })
-        }
+  const handleRegistration = async (data: z.infer<typeof formSchema>) => {
+    try {
+      const response = await fetch('/api/registration', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(data),
+      });
+
+      if (response.ok) {
+        toaster.create({
+          title: 'Registration Successful',
+          description: 'You have been registered successfully',
+          type: 'success',
+        });
+        setTimeout(() => {
+          router.push('/');
+        }, 1000);
+      } else {
+        toaster.create({
+          title: 'Registration Failed',
+          description: 'An error occurred while registering',
+          type: 'error',
+        });
+      }
+    } catch (error) {
+      console.error('Registration error:', error);
+      toaster.create({
+        title: 'Registration Failed',
+        description: 'An error occurred while registering',
+        type: 'error',
+      });
     }
-    return (        
-    <Stack direction="column" gap={4} className = "w-1/2 mx-auto max-sm:w-full max-sm:px-4">
+  };
+
+  const onSubmit = (data: z.infer<typeof formSchema>) =>
+    handleRegistration(data);
+
+  return (
+    <Stack
+      direction="column"
+      gap={4}
+      className="w-1/2 mx-auto max-sm:w-full max-sm:px-4"
+    >
+      <Toaster />
+      <form onSubmit={handleSubmit(onSubmit)}>
         <Field.Root>
           <Field.Label>Email</Field.Label>
-          <Input type="text" value={user?.email} disabled/>
+          <Input type="text" disabled {...register('email')} />
         </Field.Root>
         <Field.Root>
           <Field.Label>Name</Field.Label>
-          <Input type="text" value={user?.name || ''} />
+          <Input type="text" {...register('name')} />
         </Field.Root>
         <Field.Root>
           <Field.Label>Username</Field.Label>
-          <Input type="text" value={user?.nickname || ''} />
+          <Input type="text" {...register('username')} />
         </Field.Root>
-        <Button onClick={handleRegistration}>Confirm</Button>
-    </Stack>)
+        <Button type="submit">Confirm</Button>
+      </form>
+    </Stack>
+  );
 }
